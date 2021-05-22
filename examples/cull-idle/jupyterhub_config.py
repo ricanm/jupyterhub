ffb411d67d6ae3416bebca110a2a3705469cdc08
ricanm <83388969+ricanm@users.noreply.github.com>
2021-05-22 03:27:51 -0400

    Delete examples directory

--- examples/cull-idle/jupyterhub_config.py
+++
@@ -1,8 +0,0 @@
-# run cull-idle as a service
-c.JupyterHub.services = [
-    {
-        'name': 'cull-idle',
-        'admin': True,
-        'command': 'python cull_idle_servers.py --timeout=3600'.split(),
-    }
-]
